907ec9bbe992e74e2a2a6eaf8812a691e6557fc4
Rian0102606 <maworyan@gmail.com>
2024-11-25 12:30:01 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var options = {
+  strings:["Content Creator", "Web Designer","Frontend Developer"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 1000, 
+  loop: true 
+};
+var typed = new Typed(".text", options);
+
